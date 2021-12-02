588a92a59edc7c45b0704dbb877de29c337c49dc
Pinchon Léo <leopinchonspa@gmail.com>
2021-12-02 16:29:04 +0100

    ajout variables police + prof élèves

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const btnNavHamb = document.querySelector('#nav-bar-hamburger')
 
 // Menu nav telephone
 const navPhone = document.querySelector('.nav-bar-phone')
-
+const body = document.querySelector('body')
 btnNavHamb.addEventListener('click', () => {
     btnNavHamb.classList.toggle('active')
     navPhone.classList.toggle('phoneVisible')
+    body.classList.toggle('phoneScrollStop')
 })
